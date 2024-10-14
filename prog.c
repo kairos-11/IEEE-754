29f41e945ab1e888a5a816be43b387fb5b01d305
Kirill <cyrillopatin@gmail.com>
2024-10-14 20:58:40 +0300

    add convert from IEEE

--- prog.c
+++ prog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
- 
+#include <math.h>
+
 
 float absolute(float num){
 	return (num < 0) ? -num : num;
@@ -60,22 +61,77 @@ void find_exp(double *num, char *res){
 }
 
 
-int main() {
-    double num;
-    scanf("%lf", &num);
-
-    int sgn_bit = (num < 0) ? 1 : 0;
-    num = absolute(num);
-    printf("%d\n", sgn_bit);
-    char bin_exp[8]; 
-    find_exp(&num, bin_exp);
-    printf("%s\n", bin_exp);
-	//printf("%lf", num);
-    
+void convert_to(double num, char *res){
+	res[0] = (num < 0) ? '1' : '0';
+	char exp[8];
+	char frac[23];
+	find_exp(&num, exp);
 	num -= (int)num;
-	char bin_frac[23];
-	frac_to_bin(num, bin_frac);
-	printf("%s\n", bin_frac);
+	frac_to_bin(num, frac);
+	printf("%s\n", exp);
+	for (int i = 0; i < 8; i++){
+		res[i+1] = exp[i];
+	}
+	for (int i = 0; i < 23; i++){
+		res[i+9] = frac[i];
+	}
+}
+
+int bin_to_int(char *bin, int sz){
+	int res = 0;
+	int pw = 1;
+	for (int i = sz-1; i >= 0; i--){
+		res += (bin[i] - '0') * pw;
+		pw *= 2;
+	}
+
+	return res;
+}
+
+float bin_to_frac(char *bin, int sz){
+	float res = 0;
+	double pw = 1.0 / 2;
+	for (int i = 0; i < sz; i++){
+		res += (bin[i] - '0') * pw;
+		pw /= 2.0;
+	}
+	return res;
+}
+
+float convert_from(int sgn_bit, char *bin_exp, char *bin_frac){
+	int sgn = (sgn_bit = 0) ? 1 : -1;
+	float frac = bin_to_frac(bin_frac, 23);
+	int exp = bin_to_int(bin_exp, 8);
+	float res = sgn * (1 + frac) * pow(2, (exp - 127));
+	return res;
+}
+
+int main() {
+	int choice;
+	printf("Введите 0, если хотите перевести вещ. число в IEEE 754, и 1 если хотите перевести из IEEE 754 в вещ.число\n");
+	scanf("%d", &choice);
+	if (choice == 0){
+	    double num;
+	    scanf("%lf", &num);
+		char res[33];
+		convert_to(num, res);
+		res[32] = '\0';
+		printf("%s\n", res);
+	} else if (choice == 1){
+		char num[32];
+		scanf("%32s", num);
+		char sgn_bit = num[0];
+		char exp[9];
+		char frac[24];
+		for (int i = 1; i < 9; i++){
+			exp[i-1] = num[i];
+		}
+		for (int i = 9; i < 32; i ++){
+			frac[i-9] = num[i];
+		}
+		printf("%f\n", convert_from(sgn_bit, exp, frac));
+	}
+	
     return 0;
 }
  
